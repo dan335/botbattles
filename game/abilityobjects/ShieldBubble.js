f5ada01de3b6409f69a11892db2d545dbf8e3f30
Dan <danphi@gmail.com>
2019-02-11 19:17:42 -0800

    shield

--- game/abilityobjects/ShieldBubble.js
+++ game/abilityobjects/ShieldBubble.js
@@ -10,13 +10,28 @@ import {
 
 
 export default class ShieldBubble extends Obj {
-  constructor(manager, x, y, rotation, radius, id) {
+  constructor(manager, x, y, rotation, radius, id, ship) {
     super(manager, x, y, rotation, radius, id);
+    this.ship = ship;
 
     var geometry = new CylinderBufferGeometry( this.radius, this.radius, 0.1, 32 );
     var material = new MeshBasicMaterial( {color: 0x356ab5} );
     this.mesh = new Mesh( geometry, material );
     this.mesh.position.set(this.position.x, 0, this.position.y);
     this.manager.scene.add(this.mesh);
+  }
+
+
+  tick() {
+    this.mesh.position.set(this.ship.position.x, 0, this.ship.position.y);
+  }
+
+
+  destroy() {
+    super.destroy();
+    const index = this.manager.abilityObjects.indexOf(this);
+    if (index != -1) {
+      this.manager.abilityObjects.splice(index, 1);
+    }
   }
 }
