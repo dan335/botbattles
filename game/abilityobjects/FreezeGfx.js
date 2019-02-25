8a3fb9b1fad77217eec797c1c837d7cd193b43a6
Dan <danphi@gmail.com>
2019-02-24 21:12:24 -0800

    gfx

--- game/abilityobjects/FreezeGfx.js
+++ game/abilityobjects/FreezeGfx.js
@@ -8,39 +8,40 @@ import {
 } from 'three';
 import Particle from '../fx/Particle.js';
 
+const color = 0x356ab5;
 
+export default class FreezeGfx extends Obj {
+  constructor(manager, radius, id, ship) {
+    super(manager, ship.position.x, ship.position.y, ship.rotation, radius, id);
+    this.ship = ship;
 
-export default class Explosion extends Obj {
-  constructor(manager, x, y, rotation, radius, id, color) {
-    super(manager, x, y, rotation, radius, id);
-    this.radius = radius;
     var geometry = new CylinderBufferGeometry( 1, 1, 0.1, 32 );
-    var material = new MeshBasicMaterial( {color: parseInt(color)} );
+    var material = new MeshBasicMaterial( {color: color} );
     this.mesh = new Mesh( geometry, material );
     this.mesh.position.set(this.position.x, 0, this.position.y);
     this.manager.scene.add(this.mesh);
 
-    if (this.radius > 30) {
-      for (let i = 0; i < 10; i++) {
-        new Particle(this.manager, {
-          x: this.position.x,
-          y: this.position.y,
-          rotation: Math.random() * Math.PI*2,
-          scale: 20,
-          speed: this.radius / 60,
-          drag: 0.8,
-          lifespan: 200,
-          color: parseInt(color),
-          fadeTime: 150
-        });
-      }
+    for (let i = 0; i < 20; i++) {
+      new Particle(this.manager, {
+        x: this.position.x,
+        y: this.position.y,
+        rotation: Math.random() * Math.PI*2,
+        scale: 20,
+        speed: 2,
+        drag: 0.8,
+        lifespan: 150,
+        color: color,
+        fadeTime: 80
+      });
     }
   }
 
 
   tick() {
+    this.mesh.position.set(this.ship.position.x, 0, this.ship.position.y);
+
     let scale = this.mesh.scale.x;
-    scale += this.radius / 5;
+    scale += this.radius / 6;
     if (scale <= this.radius) {
       this.mesh.scale.set(scale, 1, scale);
     } else {
