89e5f5925f15309ee0e67efb6e28499d8d4aafa5
Dan <danphi@gmail.com>
2019-02-25 20:28:06 -0800

    vacuum

---
+++ game/abilityobjects/VacuumEffect.js
@@ -0,0 +1,45 @@
+import Particle from '../fx/Particle.js';
+import Obj from '../Obj.js';
+
+const color = 0x356ab5;
+
+export default class VacuumEffect extends Obj {
+  constructor(manager, radius, id, ship) {
+    super(manager, ship.position.x, ship.position.y, ship.rotation, radius, id);
+    this.ship = ship;
+    this.particles = [];
+  }
+
+
+  tick() {
+
+    for (let i = 0; i < 5; i++) {
+      const rotation = Math.random() * Math.PI*2;
+
+      new Particle(this.manager, {
+        x: this.ship.position.x + Math.cos(rotation) * this.radius,
+        y: this.ship.position.y + Math.sin(rotation) * this.radius,
+        rotation: rotation + Math.PI,
+        scale: 10,
+        speed: 2,
+        drag: 0.8,
+        lifespan: 100,
+        color: color,
+        fadeTime: 100
+      });
+    }
+  }
+
+
+  destroy() {
+    this.particles.forEach((particle) => {
+      particle.destroy();
+    })
+
+    super.destroy();
+    const index = this.manager.abilityObjects.indexOf(this);
+    if (index != -1) {
+      this.manager.abilityObjects.splice(index, 1);
+    }
+  }
+}
