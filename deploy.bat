1ceb527e4b4a7d4b339bba20b7e54e6a65828601
Dan <danphi@gmail.com>
2022-05-14 16:31:56 -0400

    asdf

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 docker login registry.gitlab.com -u danphi
-docker build -t registry.gitlab.com/danphi/nimp .
-docker push registry.gitlab.com/danphi/nimp
+docker build -t registry.gitlab.com/danphi/arena .
+docker push registry.gitlab.com/danphi/arena
 ssh root@botbattles.io "cd /shipyard/compose; docker-compose pull; docker-compose up -d"
